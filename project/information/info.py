cf0d99bb198cf465cbe2f11960f2d03fa0f675d2
vukter5555 <vhminkov@gmail.com>
2025-04-05 22:41:21 +0300

    Update info.py

--- project/information/info.py
+++ project/information/info.py
@@ -1,14 +1,20 @@
 import psutil
-import platform
+import cpuinfo
+
 
 def return_info():
+    # Get CPU usage
     cpu_usage = psutil.cpu_percent(interval=1)
+    
+    # Get RAM usage
     ram = psutil.virtual_memory()
     ram_percent = ram.percent
-    cpu_model = platform.processor()
-
-
-
+    
+    # Get CPU model information
+    cpu = cpuinfo.get_cpu_info()
+    cpu_model = cpu.get('brand_raw', 'Unknown CPU Model')  # Using 'brand_raw' as it's more consistent
+    
+    # Return information in a dictionary
     information = {
         'cpu_usage': f"{cpu_usage}%",
         'ram_usage': f"{ram_percent}%",
